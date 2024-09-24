441e63a38dd3698e0beedc66c179251bd9fb64d9
Tayyab Atiq <tayyabatiq300@gmail.com>
2024-09-24 09:35:09 +0500

    SDA Tassks

---
+++ SDA/Labs/Lab 1 principles/SRP.java
@@ -0,0 +1,22 @@
+// Class only responsible for generating the report
+class Report {
+    private String content;
+
+    public Report(String content) {
+        this.content = content;
+    }
+
+    public String generate() {
+        return "Report Content: " + content;
+    }
+}
+
+// Class responsible for saving the report to a file
+class FileSaver {
+    public void saveToFile(String content, String fileName) throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(content);
+        writer.close();
+    }
+}
+
